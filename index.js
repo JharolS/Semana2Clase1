7e73249a16c3e94936f52620aad0465c93bc2ecf
unknown <sv73107535@idat.edu.pe>
2021-09-01 20:50:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+let array = [1,2,3,4,5];
+
+let numbers = [1, {1: 1}, '1' ];
+
+let ocean1 = 'Pacific';
+let ocean2 = 'Atlantic';
+let ocean3 = 'Indian';
+let ocean4 = 'Southerm';
+let ocean5 = 'Artic';
+
+let oceans = ['Pacific','Atlantic','Indian','Southerm','Artic']
+
+console.log(oceans[0])
+
+let sharks = new Array(
+    "Hamm",
+    "Great White",
+    "Tiger"
+);
+
+let seaCreatures = [
+    'Octopus', //0
+    'squid', //1
+    'shark', //2
+    'whale', //3
+    'dolphin', //4
+    'fishes' //5
+]
+
+// replace
+seaCreatures[1] = 'fish'
+
+console.log(seaCreatures)
+let sharkPosition = seaCreatures.indexOf('shark',2);
+
+console.log(sharkPosition)
+
+const lastIndex = seaCreatures.length -1;
+console.log(seaCreatures[lastIndex])
+
+let nestedArray = [
+    [0,1,2], //0
+    [4,5,6]  //1
+]
+
+console.log(nestedArray[0][1])
+
+seaCreatures.push('Tiger fish');
+
+seaCreatures.unshift('Seahorse');
+
+//add
+seaCreatures[9] ='whale';
+
+//remove
+seaCreatures.splice(8,1);
+
+console.log(seaCreatures);
+
+//let newArray = [...seaCreatures]
+
+//let firstArray = seaCreatures.splice (0,3);
+//let secondArrya = seaCreatures.splice (4,2);
+
+//let fullArrays = firstArray.concat(secondArrya);
+
+//console.log('concet', fullArrays)
+
+//remove last element
+seaCreatures.pop();
+
+//reomve first element
+
+seaCreatures.shift();
+
+console.log(seaCreatures)
+
+let shellfish  = [
+    'oyster',
+    'clam',
+    'mussel',
+    'scallop',
+    'lobster',
+];
+
+for (let i = 0; i < shellfish.length; i++) {
+    console.log(i, shellfish[i])
+}
+
+let mammals = [
+    'dolphin',
+    'whale',
+    'manatee'
+];
+
+// Loop through each mammal
+
+for (let mammal of mammals) {
+    console.log(mammal)
+}
